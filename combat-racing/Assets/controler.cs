6057c4827f5d7bca16d37760dfa8523389f5906b
Krenar Shabani <krenarshabani@gmail.com>
2017-08-21 01:32:44 -0400

    character controls and camera movement (not ideal for a racing game)

---
+++ combat-racing/Assets/controler.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class controler : MonoBehaviour
+{
+    //public Transform lowerbody;
+    
+    public float speed = 10.0f;
+    float jumpSpeed = 10.0f;
+    float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private Vector3 forward = Vector3.zero;
+    private Vector3 right = Vector3.zero;
+    public Camera gCam;
+    public GameObject bomber;
+    private Quaternion preserve = new Quaternion();
+    // Use this for initialization
+    void Start()
+    {
+
+        Component[] test;
+        test = GetComponentsInChildren<BoxCollider>();
+        //print(test.Length);
+        //print(test[0].name + " " + test[1].name);
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //punchCollider.transform.position += new Vector3(0, 0, .1f);
+       // punchCollider.transform.position += new Vector3(0, 0, -.1f);
+        forward = gCam.transform.TransformDirection(Vector3.forward);
+        forward.y = 0;
+        forward = forward.normalized;
+
+        right = new Vector3(forward.z, 0, -forward.x);
+
+        if (controller.isGrounded)
+        {
+
+            //  ani.Play("Default Take" , -1, 0f);
+            // moveDirection = (Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward).normalized;
+            //transform.rotation.SetLookRotation(right);
+            moveDirection.x = (Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward).x * speed;
+            moveDirection.z = (Input.GetAxis("Vertical") * forward + Input.GetAxis("Horizontal") * right).z * speed;
+
+            //o moveDirection = transform.TransformDirection(moveDirection);
+            // transform.rotation = moveDirection;
+            //  moveDirection = moveDirection * speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //Debug.Break();
+                moveDirection.y = jumpSpeed;
+            }
+
+           // if (Input.GetKeyDown(KeyCode.F) && !ani.GetCurrentAnimatorStateInfo(0).IsName("punching") && ani.GetAnimatorTransitionInfo(0).normalizedTime < 0.10f )
+           // {
+            //    ani.SetTrigger("punch");
+                //punchCollider.isTrigger = true;
+           // }
+        
+
+            
+        }
+        else {
+            //moveDirection = (Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward+ Vector3.up*moveDirection.y).normalized;
+            moveDirection.x = (Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward).x * speed;
+            moveDirection.z = (Input.GetAxis("Vertical") * forward + Input.GetAxis("Horizontal") * right).z * speed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        //transform.rotation.Set(0, 0, 0, 0);
+
+        if (  ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ) || ( Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) )  )  
+        {
+            //if (!ani.GetCurrentAnimatorStateInfo(0).IsName("punching") && !ani.GetBool("ratatat"))
+            //{
+                transform.rotation = Quaternion.Lerp(preserve, Quaternion.LookRotation(Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward), 12f * Time.deltaTime);
+            //}
+
+           // controller.Move(moveDirection * Time.deltaTime);
+           // ani.Play("walking", -1, 0f);
+        }
+        else
+        {
+            transform.rotation = preserve;
+           // ani.Play("idle", -1, 0f);
+        }
+
+         
+         
+            controller.Move(moveDirection * Time.deltaTime);
+
+
+
+
+        //print(transform.rotation + " " + Input.GetAxis("Vertical") + " " + Input.GetAxis("Horizontal"));
+       // preserve = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+        preserve = transform.rotation;
+
+        //transform.rotation.SetLookRotation((Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward));
+        if (Input.GetKey(KeyCode.I))
+            SceneManager.LoadScene("inventory");
+
+
+        }
+    }
+
+
+
+            
+            
---
+++ combat-racing/Assets/thirdPersonCam.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class thirdPersonCam : MonoBehaviour {
+
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private const float Y_ANGLE_MIN = -40f;
+    private const float Y_ANGLE_MAX = 40f;
+
+    private const float MAX_ZOOM = 2.5f;
+    private const float MIN_ZOOM = 30f;
+
+    //private float shiftx = 2f;
+    private float shiftz = 10f;
+    public bool mouse = false;
+    //private Camera cam;
+    float dis = 8f;
+    Vector3 dest;
+   // private float distance = 15f;
+    private float currentX = 0;
+    private float currentY = 0f;
+    public float sensivityX = 4f;
+    public float sensivityY = 1f;
+
+    private Animator ani;
+
+    private void Start()
+    {
+        //Vector3 dir = new Vector3(0, 0, -distance);
+        // Quaternion rotation = Quaternion.Euler(currentY * sensivityY, currentX * sensivityX, 0);
+        //ani = GameObject.Find("Jackle").GetComponent<Animator>();
+        dis = MIN_ZOOM;
+        camTransform = transform;
+        //cam = Camera.main;
+
+    }
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        //ani.SetFloat("InputMouseH", (currentX %90 - (lookAt.transform.eulerAngles.y/4)) );
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        dis += Input.GetAxis("Mouse ScrollWheel");
+        dis = Mathf.Clamp(dis, MAX_ZOOM, MIN_ZOOM);
+        //print(dis);
+    }
+    void OnMouseDown()
+    {
+        mouse = true;
+    }
+
+    void OnMouseUp()
+    {
+        mouse = false;
+    }
+
+    private void LateUpdate()
+    {
+        //RaycastHit hit;
+        Vector3 dir;
+        if (!mouse)
+            dir = new Vector3(0, 0, -dis);
+        else
+        {
+            dir = new Vector3(0, 0, -dis - shiftz);
+        }
+        //dir += -Vector3.forward;
+        Quaternion rotation = Quaternion.Euler(currentY * sensivityY, currentX * sensivityX, 0);
+        dest = lookAt.position + rotation * dir;
+
+        //if (Physics.Linecast(camTransform.position, dest, out hit))
+       // {
+       //     camTransform.position = Vector3.Lerp(camTransform.position, hit.point +hit.normal* 0.5f, 20f * Time.deltaTime);
+        //}else
+            camTransform.position = Vector3.Lerp(camTransform.position, dest, Time.deltaTime * 20f);
+
+        // if(currentY>= lookAt.position.y)
+        //  camTransform.LookAt(lookAt.position+Vector3.up*(Mathf.Sqrt(currentY/lookAt.position.y))/2 );
+        //else
+        //camTransform.LookAt(lookAt.position + 3f*Vector3.up);
+        camTransform.LookAt(lookAt.position + 4f * Vector3.up );
+
+        camTransform.Rotate(-10f,0f, 0f);
+      //  ani.SetFloat("InputMouseH", ( Mathf.Clamp(camTransform.rotation.y - lookAt.rotation.y , -.3f, .3f)));
+        
+       // print(currentY);
+    }
+
+
+}
+
